2547432db60ce04a8a2c94a39dcaa40de6a60ea5
oshio-94 <90323949+oshio-94@users.noreply.github.com>
2022-12-21 09:05:35 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Defines a square """
 class Square:
-    """Class Square that defines a square object
-    """
+    """Defines a square object"""
     def __init__(self, size):
         """Initialize method that stores the size of the square
 
