53df1d36219437c81ade33ca9f65bfa8c10a9f29
John Ikhanoba <oshioi@yahoo.co.uk>
2023-03-13 08:00:03 -0700

    my wayward son

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 let myVar = 'C is fun'
 let yourVar = 'Python is cool'
 let ourVar = 'JavaScript is amazing'
-console.log(myVar)
-console.log(yourVar)
-console.log(ourVar)
+console.log(myVar);
+console.log(yourVar);
+console.log(ourVar);
