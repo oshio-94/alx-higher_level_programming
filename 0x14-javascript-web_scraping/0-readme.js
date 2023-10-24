6ea8015bbabe1fcfc0488df80affe1eb4c1b7777
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 11:36:35 +0000

    new updates

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 
-fs.readFile('proces.argv[2]', 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
 	if (err) throw err;
 
 	console.log(data);
