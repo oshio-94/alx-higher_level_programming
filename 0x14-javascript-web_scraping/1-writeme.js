3a938d3bf01bce3dd4b25076b378dee4ac883fab
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 12:24:54 +0000

    new updates

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+let finput = process.argv[3]
+fs.writeFile(process.argv[2], finput, (err) => {
+	if (err) throw err;
+	else{
+		console.log("The file has been updated")
+	}
+})
