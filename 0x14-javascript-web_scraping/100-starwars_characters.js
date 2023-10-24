2036f5d3b180b1983cbad463cd207ff35e98a80f
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 19:03:25 +0000

    new updates

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-'http://swapi.co/api/films/' + process.argv[2];
-
 request('http://swapi.co/api/films/' + process.argv[2], (error, response, body) => {
   if (error) throw error;
   const resp = JSON.parse(body);
