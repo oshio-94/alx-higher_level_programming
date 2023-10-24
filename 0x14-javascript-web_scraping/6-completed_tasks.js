66d9843960eb3c28d0ba7b09d0980c3a4444691a
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 18:41:12 +0000

    new updates

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,11 +7,11 @@ request(process.argv[2], (error, response, body) => {
     const resp = {};
     const json = JSON.parse(body);
     for (let i = 0; i < json.length; i++) {
-      if (json.completed === true) {
-        if (resp[json.userId] === undefined) {
-          resp[json.userId] = 0;
+      if (json[i].completed === true) {
+        if (resp[json[i].userId] === undefined) {
+          resp[json[i].userId] = 0;
         }
-        resp[json.userId]++;
+        resp[json[i].userId]++;
       }
     }
     console.log(resp);
