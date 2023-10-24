941ba76356feca9782451835f13f95168632de9f
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 14:21:40 +0000

    new updates

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/'
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 request(url + process.argv[2] + '/', (error, response, body) => {
   if (error) throw error;
-  else{ const res = JSON.parse(body);
-  console.log(res.title);
-}});
- 
+  else {
+    const res = JSON.parse(body);
+    console.log(res.title);
+  }
+});
