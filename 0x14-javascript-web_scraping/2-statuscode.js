a2ca9b74bea3a0a800c93190dc4e98bb37c18398
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 12:55:32 +0000

    new updates

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+let url = process.argv[2];
+request(url, (err, response, body) => {
+	if (err) throw err;
+	console.log(response.statusCode);
+});
